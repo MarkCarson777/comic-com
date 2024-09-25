9bf8548ae321898870f4f506fa42aa43506e229e
Mark Carson <markcarson121@gmail.com>
2024-09-25 16:38:03 -0300

    added formatters to utils

---
+++ src/utils/formatters.ts
@@ -0,0 +1,15 @@
+const CURRENCY_CONVERTER = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 0,
+});
+
+export function formatCurrency(amount: number) {
+  return CURRENCY_CONVERTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
