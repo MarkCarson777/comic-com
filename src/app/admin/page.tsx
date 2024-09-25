1b32a817a95089b77c3e658e2f94f2e941c6485c
Mark Carson <markcarson121@gmail.com>
2024-09-25 16:32:54 -0300

    created asyncronous server function to retrieve sales data from prisma database

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,28 @@
+import prisma from "@/db/db";
 import DashboardCard from "@/components/DashboardCard";
 
-export default function AdminDashboard() {
+async function getSalesData() {
+  const data = await prisma.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+export default async function AdminDashboard() {
+  const salesData = await getSalesData();
+
   return (
     <div className="md:grid-cold-2 grid grid-cols-1 gap-4 lg:grid-cols-3">
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={salesData.numberOfSales}
+        body={salesData.amount}
+      />
     </div>
   );
 }
