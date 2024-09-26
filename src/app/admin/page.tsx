5fca489131480d96e5336982ed2eb4e60b45d006
Mark Carson <markcarson121@gmail.com>
2024-09-26 10:42:35 -0300

    added userData and productData to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -14,8 +14,41 @@ async function getSalesData() {
   };
 }
 
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    prisma.user.count(),
+    prisma.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    prisma.product.count({ where: { isAvailableForPurchase: true } }),
+    prisma.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+
+  return {
+    activeCount,
+    inactiveCount,
+  };
+}
+
 export default async function AdminDashboard() {
-  const salesData = await getSalesData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
 
   return (
     <div className="md:grid-cold-2 grid grid-cols-1 gap-4 lg:grid-cols-3">
@@ -23,6 +56,16 @@ export default async function AdminDashboard() {
         title="Sales"
         subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
         body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatNumber(userData.averageValuePerUser)} Average Value`}
+        body={formatCurrency(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive `}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   );
