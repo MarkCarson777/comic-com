30da69e68c5fca60f6ca09f62ab52f1948ac9d9c
Mark Carson <markcarson121@gmail.com>
2024-09-27 09:58:14 -0300

    set up button variants

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 
+import Button from "@/components/Button";
 import PageHeader from "./_components/PageHeader";
 
 export default function AdminProductsPage() {
   return (
     <div className="flex items-center justify-between gap-4">
       <PageHeader>AdminProductsPage</PageHeader>
-      <Button>
+      <Button variant="default" size="default">
         <Link href="/admins/products/new">Add Product</Link>
       </Button>
     </div>
