9d558e129d4cfee96a8057b04e59b60ee8d1625d
Mark Carson <markcarson121@gmail.com>
2024-09-30 11:43:23 -0300

    added ProductsTable to admin/products

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -2,6 +2,13 @@ import Link from "next/link";
 
 import Button from "@/components/Button";
 import PageHeader from "./_components/PageHeader";
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+} from "@/components/ui/table";
 
 export default function AdminProductsPage() {
   return (
@@ -10,6 +17,28 @@ export default function AdminProductsPage() {
       <Button asChild>
         <Link href="/admins/products/new">Add Product</Link>
       </Button>
+      <ProductsTable />
     </div>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
+    </Table>
   );
 }
