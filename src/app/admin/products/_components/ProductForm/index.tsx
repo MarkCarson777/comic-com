e858d99173c8c3142adecefc4f6a6b02b0bde39d
Mark Carson <markcarson121@gmail.com>
2024-09-30 12:23:08 -0300

    created ProductForm

--- src/app/admin/products/_components/ProductForm/index.tsx
+++ src/app/admin/products/_components/ProductForm/index.tsx
@@ -1,5 +1,46 @@
 "use client";
 
+import { useState } from "react";
+
+import Button from "@/components/Button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { formatCurrency } from "@/utils/formatters";
+import { Textarea } from "@/components/ui/textarea";
+
 export function ProductForm() {
-  return <h1>ProductForm</h1>;
+  const [priceInCents, setPriceInCents] = useState<number>();
+
+  return (
+    <form action className="space-y-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price In Cents</Label>
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div className="text-muted-foreground">
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <Button type="submit">Save</Button>
+    </form>
+  );
 }
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import Button from "@/components/Button";
-import PageHeader from "./_components/PageHeader";
+import { PageHeader } from "./_components/PageHeader";
 import {
   Table,
   TableHead,
