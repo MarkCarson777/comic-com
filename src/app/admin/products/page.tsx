bbe35b7c0400c5b32961e317de764ad1816d3049
Mark Carson <markcarson121@gmail.com>
2024-10-23 10:14:17 -0300

    fixed layout issue

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -27,13 +27,15 @@ import {
 
 export default function AdminProductsPage() {
   return (
-    <div className="flex items-center justify-between gap-4">
-      <PageHeader>AdminProductsPage</PageHeader>
-      <Button asChild>
-        <Link href="/admin/products/new">Add Product</Link>
-      </Button>
+    <>
+      <div className="flex items-center justify-between gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
       <ProductsTable />
-    </div>
+    </>
   );
 }
 
