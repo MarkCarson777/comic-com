b0bf12d93e22468507f234e2bc073b07c768e01c
Mark Carson <markcarson121@gmail.com>
2024-10-30 15:06:17 -0300

    created CheckoutForm

---
+++ src/app/(customer)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,8 @@
+type CheckoutFormProps = {
+  product: {};
+  clientSecret: string;
+};
+
+export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
+  return <h1>Hello there</h1>;
+}
