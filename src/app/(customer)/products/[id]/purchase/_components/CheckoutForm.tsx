84da33a2ed85e28d68010a1952f617f763859d16
Mark Carson <markcarson121@gmail.com>
2024-10-30 15:27:54 -0300

    installed react stripe

--- src/app/(customer)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customer)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,8 +1,14 @@
+import { loadStripe } from "@stripe/stripe-js";
+
 type CheckoutFormProps = {
   product: {};
   clientSecret: string;
 };
 
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string,
+);
+
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
-  return <h1>Hello there</h1>;
+  return <h1>Form</h1>;
 }
