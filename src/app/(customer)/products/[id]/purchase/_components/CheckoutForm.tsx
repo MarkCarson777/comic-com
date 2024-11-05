991e9265ef0f647e947ad9b15b9e74743d9558f8
Mark Carson <markcarson121@gmail.com>
2024-11-05 09:50:05 -0300

    displayed product details on checkout page

--- src/app/(customer)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customer)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -7,9 +7,16 @@ import {
   useStripe,
   PaymentElement,
 } from "@stripe/react-stripe-js";
+import Image from "next/image";
+import { formatCurrency } from "@/lib/formatters";
 
 type CheckoutFormProps = {
-  product: {};
+  product: {
+    imagePath: string;
+    name: string;
+    description: string;
+    priceInCents: number;
+  };
   clientSecret: string;
 };
 
@@ -19,9 +26,32 @@ const stripePromise = loadStripe(
 
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
   return (
-    <Elements options={{ clientSecret }} stripe={stripePromise}>
-      <Form />
-    </Elements>
+    <div>
+      <div className="mx-auto w-full max-w-5xl space-y-8">
+        <div className="flex items-center gap-4">
+          <div className="relative aspect-video w-1/3 flex-shrink-0">
+            <Image
+              src={product.imagePath}
+              fill
+              alt={product.name}
+              className="object-cover"
+            />
+          </div>
+          <div>
+            <div className="text-lg">
+              {formatCurrency(product.priceInCents / 100)}
+            </div>
+            <h1 className="text-2xl font-bold">{product.name}</h1>
+            <div className="line-clamp-3 text-muted-foreground">
+              {product.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form />
+      </Elements>
+    </div>
   );
 }
 
